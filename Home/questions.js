53e771bcd146501dc6ca94ff80e9982506d15c56
Priyanshi Agrawal <131484218+PriyanshiiAgrawall@users.noreply.github.com>
2024-02-22 14:18:39 +0530

    Added All The necessary files

---
+++ Home/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "According to Ayurveda, what are the three doshas that govern human physiology and psychology?",
+        answer: "Vata, Pitta, Kapha",
+        options: [
+            "Vata, Pitta, Kapha",
+            "Yin, Yang, Qi",
+            "Sattva, Rajas, Tamas",
+            "Agni, Ama, Ojas"
+        ]
+    },
+    {
+        numb: 2,
+        question: "In Ayurveda, what is the concept of ",
+        answer: "Digestive fire",
+        options: [
+            "Digestive fire",
+            "Life force energy",
+            "Mind-body balance",
+            "Physical strength",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of the following is considered the primary aim of Ayurveda?",
+        answer: "Preventing diseases",
+        options: [
+            "Treating symptoms",
+            "Preventing diseases",
+            "Achieving physical strength",
+            "Balancing the chakras",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following herbs is commonly used in Ayurvedic medicine to promote relaxation and reduce stress?",
+        answer: "Ashwagandha",
+        options: [
+            "Ashwagandha"
+            , "Turmeric"
+            , "Ginseng"
+            , "Echinacea"
+        ]
+    },
+    {
+        numb: 5,
+        question: "According to Ayurveda, what is the primary cause of disease?",
+        answer: "Imbalance of the doshas",
+        options: [
+            "Imbalance of the doshas",
+            "Poor diet",
+            "Lack of exercise",
+            "Genetic predisposition",
+        ]
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
